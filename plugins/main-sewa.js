1576149285956f3f199f82160e8d788a3b8b9ff3
pangky1 <110285695+pangky1@users.noreply.github.com>
2022-10-24 15:25:46 +0700

    Update main-sewa.js

--- plugins/main-sewa.js
+++ plugins/main-sewa.js
@@ -9,15 +9,15 @@ let handler = async (m, { conn }) => {
 ┌「 *Sewa bot* 」
 │     
 ├ 1 Grup / 30 Hari
-├ Rp. 10,000 Gopay
-├ Rp. 15,000 Pulsa Three
+├ Rp. 15,000 Gopay, Dana
+├ Rp. 20,000 Pulsa Indosat & Telkomsel 
 │
 ├ 1 Premium / 30 Hari
 ├ Rp. 5,000 Gopay
 ├ Rp. 10,000 Pulsa Three
 │
 ├ tertarik? hubungi: 
-├ @62895336282144 (The.sad.boy01)
+├ @6285779386736 (@iam_syap)
 └────
 `.trim(),
         "listType": "PRODUCT_LIST",
@@ -36,14 +36,14 @@ let handler = async (m, { conn }) => {
                 "productId": "4730029423700586",
                 "jpegThumbnail": baper
             },
-            "businessOwnerJid": "6288217277973@s.whatsapp.net"
+            "businessOwnerJid": "6285779386736@s.whatsapp.net"
         },
-        "footerText": "https://wa.me/c/6288217277973\n\nowner number : wa.me/62895336282144"
+        "footerText": "owner number : wa.me/6285779386736"
     }
     conn.sendMessage(m.chat, listMessage, 'listMessage', { quoted: m, contextInfo: { mentionedJid: conn.parseMention(listMessage.description) } })
 }
-handler.help = ['sewabot', 'premium']
+handler.help = ['sewabot', 'premium', 'sewa']
 handler.tags = ['main']
-handler.command = /^(sewabot|premium)$/i
+handler.command = /^(sewabot|premium|sewa)$/i
 
 module.exports = handler
